f59f97b4dba259e7ac99e9e1a56117db7b400783
Anton Khomenko <toxaestel@gmail.com>
2016-05-07 23:35:57 +0300

    l1 posts JSON and striped

---
+++ client_src/post.js
@@ -0,0 +1,39 @@
+(function ($) {
+    $(document).ready(function () {
+
+        Handlebars.registerHelper('JSON', function() {
+            var string = JSON.stringify(this.posts, null, '\t');
+            
+            return new Handlebars.SafeString(string);
+        });
+
+        Handlebars.registerHelper('striped', function () {
+            var result = '<table class="striped">';
+            $(this.posts).each(function (ignore, el) {
+                result += '<tr><td>' + el.description + '</td></tr>';
+            });
+            result += '</table>';
+            return result;
+        });
+
+        function render() {
+            var posts = {
+              'posts': Data.getPosts()
+            };
+            renderTemplate('JSONPosts', 'template-JSON-posts', posts);
+            renderTemplate('stripedPosts', 'template-striped-posts', posts);
+        }
+
+        function compileTemplate(dataObj, templateId) {
+            var compiler = Handlebars.compile($('#'+templateId).html());
+
+            return compiler(dataObj);
+        }
+
+        function renderTemplate(elId, templateId, data) {
+            $('#'+elId).append(compileTemplate(data, templateId));
+        }
+
+        render();
+    });
+})(jQuery);
